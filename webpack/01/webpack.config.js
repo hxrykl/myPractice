31973e949e906b11ac5ab6f3188e8397ee99a1a9
= <1574551580@qq.com>
2020-08-28 00:48:43 +0800

    webpack安装与打包

--- webpack/01/webpack.config.js
+++ webpack/01/webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require('path');//
 
 module.exports = {
-	entry: './index.js',
-	output: {
-		filename: 'bundle.js',
-		path: 'bundle'
+	entry: './index.js',//打包开始文件
+	output: { //打包好的文件放至
+		filename: 'bundle.js',//打包好的文件命名
+		path: path.resolve(__dirname, 'bundle1')//打包好的文件存放的文件夹，必须是绝对路径；__dirname代指当前文件下的绝对路径，bundle指定文件夹名
 	}
 }
