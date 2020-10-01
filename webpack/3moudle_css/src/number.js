9e6266a3157e30a81b819e7de256fec76ad47664
= <1574551580@qq.com>
2020-10-01 12:54:11 +0800

    hotModuleReplacementPlugin

---
+++ webpack/3moudle_css/src/number.js
@@ -0,0 +1,7 @@
+function number () {
+	var div = document.createElement('div');
+	div.setAttribute('id', 'number');
+	div.innerHTML = 210011
+	document.body.appendChild(div);
+}
+export default number;
