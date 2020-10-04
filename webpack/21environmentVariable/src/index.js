1a75a69051f7767abb19d0dfd806749623bfdf93
= <1574551580@qq.com>
2020-10-05 03:49:26 +0800

    环境变量 env

---
+++ webpack/21environmentVariable/src/index.js
@@ -0,0 +1,12 @@
+// import _ from 'lodash';
+// import $ from 'jquery';
+// import {jqueryFunc} from './jqueryFunc.js';
+// const dom = $('<div>');
+// dom.html(_.join(['not', 'wanwan', ' ']));
+// $('body').append(dom);
+
+
+// jqueryFunc();
+
+
+console.log(this,'this')
