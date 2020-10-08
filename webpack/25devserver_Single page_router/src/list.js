2d118cc802935d2b373d5351ef52cffa031364e5
= <1574551580@qq.com>
2020-10-09 02:40:09 +0800

    DevServer解决单页面应用路由问题

---
+++ webpack/25devserver_Single page_router/src/list.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class List extends Component{//创建一个可以渲染出div内容的组件
+	
+	render() {
+		return <div>ListPage</div>
+	}
+}
+
+export default List;
---
+++ webpack/3moudle_css/src/home.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Home extends Component{//创建一个可以渲染出div内容的组件
+	
+	render() {
+		return <div>HomePage</div>
+	}
+}
+
+export default Home;
