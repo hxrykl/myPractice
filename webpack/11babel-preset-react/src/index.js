b9b3720fe541e9043c52e2bd7293911d72d60a24
万伟亮 <wanweiliang@91cyt.com>
2020-10-08 15:55:28 +0800

    DevServer proxy 实现请求转发

--- webpack/11babel-preset-react/src/index.js
+++ webpack/11babel-preset-react/src/index.js
@@ -1,10 +1,21 @@
 // import "@babel/polyfill";
 // import 'regenerator-runtime/runtime';//babel7.4之后替代@babel/polyfill
 
+import axios from 'axios';
+
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
 class App extends Component{//创建一个可以渲染出div内容的组件
+	componentDidMount() {//页面加载完毕执行
+		axios.get('/react/api/header.json')
+		.then(res => {
+			console.log(res)
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	}
 	render() {
 		return <div>hello world</div>
 	}
--- webpack/3moudle_css/src/index.js
+++ webpack/3moudle_css/src/index.js
@@ -1,15 +1,24 @@
-console.log('hello, this is dell');
+// import "@babel/polyfill";
+// import 'regenerator-runtime/runtime';//babel7.4之后替代@babel/polyfill
 
+import axios from 'axios';
 
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
 
-if ('serviceWorker' in navigator) {//判断浏览器是否支持serviceWorker，
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('./service-worker.js')//应用serviceWorker，
-		.then(registration => {
-			console.log('service-worker registed')
+class App extends Component{//创建一个可以渲染出div内容的组件
+	componentDidMount() {//页面加载完毕执行
+		axios.get('/react/api/header.json')
+		.then(res => {
+			console.log(res)
 		})
 		.catch(err => {
-			console.log('service-worker registed error')
+			console.log(err)
 		})
-	})
+	}
+	render() {
+		return <div>hello world</div>
+	}
 }
+
+ReactDom.render(<App />, document.getElementById('root'));//将组件挂载到页面
