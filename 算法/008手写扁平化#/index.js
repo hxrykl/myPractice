a3574205d2fe2a028c087da5d00e09b6b2f7566b
万伟亮 <wanweiliang@91cyt.com>
2020-11-26 14:49:53 +0800

    手写扁平化

---
+++ 算法/008手写扁平化#/index.js
@@ -0,0 +1,16 @@
+/*
+手写扁平化函数
+*/
+
+
+var newArr =  []
+function myFlat(arr, n) {
+	arr.map((item, index) => {
+		if(item.constructor === Array && n > 1){
+			myFlat(item, n-1)
+		} else {
+			newArr.push(item)
+		}
+	})
+	return newArr
+}
