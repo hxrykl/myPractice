abba2f48f7545ac7742d613b582b0d04e0657a55
hxrykl <1574551580@qq.com>
2021-02-24 12:33:07 +0800

    内循环解法

--- 算法/022删除字符串中出现次数大于或等于2的相邻字符#/index.js
+++ 算法/022删除字符串中出现次数大于或等于2的相邻字符#/index.js
@@ -5,6 +5,7 @@
 输出： ‘ca’
 
 */
+/*利用栈加标记*/
 var removeDuplicates = function(s) {
     let stack = [], mark = ''
     for(let c of s){
@@ -20,3 +21,21 @@ var removeDuplicates = function(s) {
     }
     return stack.join('')
 };
+
+
+var removeDuplicates = function(s) {
+    let stack = [], prev = '', next = '',i = 0
+    while(i < s.length){
+    	prev = stack[stack.length - 1]
+    	next = s[i]
+    	if(prev === next){
+    		stack.pop()
+    		while(s[i] === top) i++ 
+    	} else {
+    		stack.push(next)
+    		i++
+    	}
+    }
+    return stack.join('')
+};
+
