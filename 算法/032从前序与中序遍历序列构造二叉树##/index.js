ddf1d47f3feb8666980fe60c9d5f3ae7c8c58655
hxrykl <1574551580@qq.com>
2021-03-25 14:10:35 +0800

    递归指针

---
+++ 算法/032从前序与中序遍历序列构造二叉树##/index.js
@@ -0,0 +1,36 @@
+/*
+从前序与中序遍历序列构造二叉树
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+*/
+
+//递归
+function buildTree(preorder, inorder) {
+
+	if(inorder.length === 0) return null
+
+	let root = new TreeNode(preorder[0])
+	let mid = inorder.indexOf(preorder[0])
+	//
+	root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
+	root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid+1))
+
+	return root
+}
+
+//指针递归
+function buildTree(preorder, inorder) {
+
+	function pointer (pre_star, pre_end, in_star, in_end) {
+		if(pre_star > pre_end) return null
+		let rootVal = preorder[pre_star]
+		let root = new TreeNode(rootVal)
+		let mid = inorder.indexOf(rootVal)
+		let leftNum = mid - in_star
+		root.left = pointer(pre_star + 1, pre_star + leftNum, in_star, mid - 1)
+		root.right = pointer(pre_star + leftNum + 1, pre_end, mid + 1, in_end)
+		return root
+	}
+	return pointer(0, preorder.length - 1, 0, inorder.length - 1)
+}
